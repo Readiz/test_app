7985f7b084761188b3c161f5a291d8c982407b32
readiz <readiz@readiz.com>
2017-08-13 21:54:42 +0900

    RND Number Ex.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import RandomNumber from './RandomNumber';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            value: Math.round(Math.random()*100)
+        };
+
+        this.updateValue = this.updateValue.bind(this);
+    }
+
+    updateValue(randomValue){
+        this.setState({
+            value: randomValue
+        });
+    }
+
     sayHey(){
         alert("hey");
      }
@@ -15,6 +34,11 @@ class App extends React.Component {
 
         return  ( /* Render Comment Test */
             <div>
+                <Header title={ this.props.headerTitle }/>
+                <Content title={ this.props.contentTitle }
+                          body={ this.props.contentBody }/>
+                <RandomNumber number={this.state.value}
+                        onUpdate={this.updateValue} />
                 <h1> Hello Readiz!</h1>
                 <h2> Welcome to {text}</h2>
                 <button onClick= {this.sayHey}>Click Me</button>
@@ -23,5 +47,11 @@ class App extends React.Component {
         );
     }
 }
+
+App.defaultProps = {
+    headerTitle: 'Default header',
+    contentTitle: 'Default contentTitle',
+    contentBody: 'Default contentBody'
+};
 
 export default App;
