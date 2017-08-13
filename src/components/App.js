77daadae01b5c7cadc952ade9363d80d8b199ac0
readiz <readiz@readiz.com>
2017-08-13 21:14:15 +0900

    Sample 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
-    render(){
+    sayHey(){
+        alert("hey");
+     }
+ 
+     render(){
+        let text = "Dev-Server";
 
-        return (
-                <h1>Hello React Skeleton</h1>
+        let pStyle = {
+            color: 'aqua',
+            backgroundColor: 'black'
+        };
+
+        return  ( /* Render Comment Test */
+            <div>
+                <h1> Hello Readiz!</h1>
+                <h2> Welcome to {text}</h2>
+                <button onClick= {this.sayHey}>Click Me</button>
+                <p style = {pStyle}>{1 == 1 ? 'True' : 'False'}</p>
+            </div>
         );
     }
 }
